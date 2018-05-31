ec7375f88d8f1bb60a5aefc68cb21fc1a261599c
tuedachu <tuedachu@gmail.com>
2018-05-31 13:56:54 +0200

    Init

---
+++ ampl-mode.el
@@ -0,0 +1,30 @@
+;;; ampl-mode.el --- Emacs major mode for AMPL files. -*- lexical-binding: t -*-
+
+;; Copyright (C) Arnaud Hoffmann <tuedachu@gmail.com>
+
+;; Author: Arnaud Hoffmann  <tuedachu@gmail.com>
+;; URL: https://github.com/tuedachu/ampl-mode
+;; Version: 1.0
+;; Package-Requires:
+;; Keywords: ampl
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; Emacs major mode for ampl files
+
+;;; Code:
+;;
+
+;;; ampl-mode.el ends here
